ea68821613d0c1fa8515216bef854168cb6ef0e3
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-23 02:31:38 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Cappuchino
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Init : BasePlugin
+    {
+        public static Init instance;
+
+        public override void Load()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            instance = this;
+
+            AddComponent<Plugin>();
+        }
+
+        public override bool Unload()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            return true;
+        }
+    }
+
+    public class Plugin : MonoBehaviour
+    {
+        void Start()
+        {
+            SceneManager.LoadScene("CapuchinCopy");
+        }
+
+        void OnGUI()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Cappuchino
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.goldentrophy.gorillatag.cappuchino";
+        public const string Name = "Cappuchino";
+        public const string Version = "1.0.0";
+    }
+}
